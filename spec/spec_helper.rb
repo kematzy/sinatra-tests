ee002d6939749f987838b5d5bc97cca29bc2ab30
kematzy <kematzy@gmail.com>
2009-10-08 17:41:43 +0800

    Initial spec_helper.rb file for testing code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,29 @@ require 'spec'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'sinatra-tests'
+require 'sinatra/tests'
 
 Spec::Runner.configure do |config|
+  config.include RspecHpricotMatchers
+  config.include Sinatra::Tests::SharedSpecs
+end
+
+
+# quick convenience methods..
+
+def fixtures_path
+  "#{File.dirname(File.expand_path(__FILE__))}/fixtures"
+end
+
+
+class MyTestApp < Sinatra::Base
+  register(Sinatra::Tests)
+  
+end
+
+class Test::Unit::TestCase
+  include Sinatra::Tests::TestCase
+  
+  Sinatra::Base.set :environment, :test
   
 end
